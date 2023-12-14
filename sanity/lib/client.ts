709fe340fba8b0def9cfad921360c943648e7f40
Ahmed Akram <akram78787@gmail.com>
2023-12-14 18:51:49 +0400

    add-project

--- sanity/lib/client.ts
+++ sanity/lib/client.ts
@@ -7,5 +7,4 @@ export const client = createClient({
   dataset,
   projectId,
   useCdn,
-  token: 'skf2JCEKEMn638CRXCCEk3fulHkyNxe5U6afp5VMKJoSNnNLKioVyUg0ypP89TVeizAWpUQlEX8RErzkpJcaLqAwRh8sbjKsxueYfCdSj6rmMcB5qWpvpHlQf3dDrNwWBLTjGB5R5KI584RqvjlFWrtdnCyUE7EUvCNLIJ8V3ujNZ9rJUuft',
 })
